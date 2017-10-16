25c581f4c32e8131b65b6e0e502862fd15c44537
honzi <czechinthemail@gmail.com>
2017-10-15 18:16:52 -0700

    moved uncommon functions to data.js

--- js/main.js
+++ js/main.js
@@ -1,68 +1,5 @@
 'use strict';
 
-function guess(){
-    if(!guessing){
-        return;
-    }
-
-    var element = document.getElementById('guess-input');
-    var guessvalue = parseInt(
-      element.value,
-      10
-    );
-
-    // If the guess is not a number or not in guessing range.
-    if(isNaN(guessvalue)
-      || guessvalue.length < 1
-      || guessvalue > core_storage_data['max']
-      || guessvalue < core_storage_data['min']){
-        document.getElementById('info').innerHTML =
-          'Invalid integer.';
-        return;
-    }
-
-    element.value = guessvalue;
-    var result = '';
-
-    // Check if valid guess is lower than value.
-    if(guessvalue > value){
-        result = 'LOWER';
-
-    // Check if valid guess is higher than value.
-    }else if(guessvalue < value){
-        result = 'HIGHER';
-
-    // Only option left is guess is correct.
-    }else{
-        guessing = false;
-        result = 'CORRECT! YOU WIN!';
-    }
-
-    element = document.getElementById('guesses');
-    element.innerHTML = parseInt(
-      element.innerHTML,
-      10
-    ) + 1;
-    document.getElementById('info').innerHTML = result;
-}
-
-function new_game(skip){
-    if(!skip
-      && !window.confirm('Start new game?')){
-        return;
-    }
-
-    document.getElementById('guess-input').value = '';
-    document.getElementById('guesses').innerHTML = 0;
-    document.getElementById('info').innerHTML = '';
-    guessing = true;
-
-    // Generate new value to guess.
-    value = core_random_integer({
-      'max': core_storage_data['max'] - core_storage_data['min'],
-    }) + core_storage_data['min'];
-}
-
 function repo_init(){
     core_repo_init({
       'keybinds': {
@@ -90,6 +27,3 @@ function repo_init(){
         new_game(false);
     };
 }
-
-var guessing = true;
-var value = 0;
