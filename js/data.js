f4dc968a375cd10241e9a11ca0be5fa04c137d0a
Jan Hořava <czechinthemail@gmail.com>
2018-07-07 13:12:47 -0700

    switched to Number.isNaN()

--- js/data.js
+++ js/data.js
@@ -11,7 +11,7 @@ function guess(){
     );
 
     // If the guess is not a number or not in guessing range.
-    if(isNaN(guessvalue)
+    if(Number.isNaN(guessvalue)
       || guessvalue.length < 1
       || guessvalue > core_storage_data['max']
       || guessvalue < core_storage_data['min']){
